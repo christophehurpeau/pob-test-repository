376a1febf68776b533405b6d922da7da8c2b0c2d
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2025-02-06 22:20:33 +0100

    chore: pob update

--- dist/index-node20.mjs
+++ dist/index-node20.mjs
@@ -4,4 +4,4 @@ const sayHello = () => {
 const aParticularlyLongAndObnoxiousNameForIllustrativePurposes = 44;
 
 export { aParticularlyLongAndObnoxiousNameForIllustrativePurposes, sayHello };
-//# sourceMappingURL=index-node18.mjs.map
+//# sourceMappingURL=index-node20.mjs.map
